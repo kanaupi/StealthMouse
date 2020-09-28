065431d2dc233893f29b218840507cea9de4a00e
金内光 <hikari00.43.baske@icloud.com>
2020-09-28 23:11:04 +0900

    ステージになるasset追加。プレイヤーの横移動実装

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+
+    Vector3 movement;
+    Quaternion rotation = Quaternion.identity;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        movement.Set(horizontal, 0f, vertical);
+        movement.Normalize();
+
+        
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, turnSpeed * Time.deltaTime, 0f);
+        rotation = Quaternion.LookRotation(desiredForward);
+        rb.MovePosition(rb.position + movement * Time.deltaTime);
+        rb.MoveRotation(rotation);
+    }
+    
+}
